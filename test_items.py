5cbb5a430e5c1182a79f4a21ab65df817e03873f
Natalia1104 <natashka-ulibashka@mail.ru>
2019-10-10 00:06:50 +0300

    Change assert

--- test_items.py
+++ test_items.py
@@ -4,4 +4,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_find_button_for_add_product(browser):
     browser.get(link)
     #time.sleep(30)
-    assert browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket"), "Button was not found for some reason!"
+    el = browser.find_elements_by_xpath("//form[@id='add_to_basket_form']/button[@type='submit']")
+    assert len(el) > 0, "Button was not found for some reason!"
